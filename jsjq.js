0e0e726b232073ea7388fa001e68375cb66f3f63
Michael Manwaring <Michael.c.Manwaring@gmail.com>
2015-12-11 14:29:30 -0500

    just starting the 'show'

--- jsjq.js
+++ jsjq.js
@@ -43,11 +43,20 @@ function secretword (word3) {
 	 return count
 }
 
-
+function testvar (q) {
+	if(q<10){
+		return 1
+	} else if(q>10){
+		return 2
+	} else if (q=10) {
+		return 3
+	};
+};
 
 
 
 $(document).ready(function(){
+
 
 
 	$("#expclick").click(function(){
@@ -73,6 +82,48 @@ $(document).ready(function(){
 		arrayprint()
 		arrayprint()
 	})
+	$(".tester").click(function(){
+		var temp = $(this).attr("id")
+		if (temp=="x"){
+			numtest= "#num4"
+		} else if (temp=="y") {
+			numtest= "#num5"
+		} else if (temp=="z") {
+			numtest= "#num6"
+		} else {
+			alert("something broke")
+		};
+		var tenornot = testvar($(numtest).val())
+		if (tenornot==2) {
+			$("#tenanswer").text("Your variable was "+temp+" and it is higher than 10")
+		}else if(tenornot==1){
+			alert("LESS THAN TEN! DO BETTER!")
+			$("#tenanswer").text("I dont even know what to say to you....")
+		}else if(tenornot=3){
+			$("body").css("background-image" , "url('ten.jpg')");
+			$("#tenanswer").text("")
+
+		}
+
+	});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 	$("#secretword").click(function(){
 		// console.log('test')
 		var letters = secretword($("#word3").val())
@@ -111,33 +162,33 @@ $(document).ready(function(){
 
 // -----------------------------------------------Javascript:
 
-// Create a script with two variables assigned to two numbers. Add them together and output the result to the console
+//xxxxxx Create a script with two variables assigned to two numbers. Add them together and output the result to the console
 
-// Try to add two strings together and output the result to an alert
+// xxxxxxxxxxxxxxxxx Try to add two strings together and output the result to an alert
 
-// Create a multi-dimensional array related to a subject that interests you. Output two of the items in sub-arrays to the console
+// xxxxxxxxxxxxxxxxxxxx Create a multi-dimensional array related to a subject that interests you. Output two of the items in sub-arrays to the console
 
-// Write a program that checks if a variable is less than 10. If it is, alert the user that their variable is less than 10. If it is not, let the user know what the variable was and that it was greater than 10.
+// xxxxxxxxxxxxxxxxxx Write a program that checks if a variable is less than 10. If it is, alert the user that their variable is less than 10. If it is not, let the user know what the variable was and that it was greater than 10.
 
-// Try running the script and then changing the variable’s value to see how this affects the program output
+// xxxxxxxxxxxxxxx Try running the script and then changing the variable’s value to see how this affects the program output
 
 // If you have extra time, write a similar program to check if a string stored in a variable is the same as another string
 
 // Declare a function that takes a name as an argument and tells the user what name they’ve entered, try running it after it has been declared
 
-// Declare a function that takes no arguments but prints something to the console, try running it after it has been declared
+// xxxxxxxxxxxxxxxxxxxx      Declare a function that takes no arguments but prints something to the console, try running it after it has been declared
 
-// Declare a function that, accepts user input (from an input element). Also create a variable in JS assigned to a word of your choice. Depending upon the length of the text the user types, tell the user how many characters off their text was from your "secret" word. 
+// xxxxxxxxxxxxxxxxx   Declare a function that, accepts user input (from an input element). Also create a variable in JS assigned to a word of your choice. Depending upon the length of the text the user types, tell the user how many characters off their text was from your "secret" word. 
 
 // --------------------------------------------------- jQuery
 
-// Create a basic HTML page with no styling that has at least 10 separate elements on it
+Create a basic HTML page with no styling that has at least 10 separate elements on it
 
-// Use jQuery to create an animated “show”! Try changing an element’s CSS, hiding, showing, fading, and sliding, and making the page run amok
+Use jQuery to create an animated “show”! Try changing an element’s CSS, hiding, showing, fading, and sliding, and making the page run amok
 
-// Once the “show” works, rig it up to only fire when the user clicks anywhere on the page
+Once the “show” works, rig it up to only fire when the user clicks anywhere on the page
 
-// If you finish this exercise, implement jQuery into some of the websites you built last week and try playing around with some other functions: http://api.jquery.com/ (Links to an external site.)
+If you finish this exercise, implement jQuery into some of the websites you built last week and try playing around with some other functions: http://api.jquery.com/ (Links to an external site.)
 
 // ---------------------------------------------------Extra Credit
 
